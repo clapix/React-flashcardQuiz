b89790b12ab45c0a17035ec743fcf6567ad6fceb
claudia <mail@claudiapiccioni.it>
2020-05-18 16:37:22 +0200

    Created a boilerplate of the needed components and added an array of objects as placeholder data, for now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>hello</h1>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "what is 2 + 2?",
+    answer: "4",
+    optons: [
+      "2",
+      "3",
+      "4",
+      "5",
+    ]
+  }
+  {
+    id: 2,
+    question: "what is a dog?",
+    answer: "an animal",
+    optons: [
+      "an object",
+      "a human being",
+      "an animal",
+      "a plant",
+    ]
+  }
+  {
+    id: 3,
+    question: "what's the shape of the sun?",
+    answer: "circle",
+    optons: [
+      "rectangular",
+      "circle",
+      "triangle",
+    ]
+  }
+]
 
 export default App;
---
+++ src/FlashCardList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FlashCardList() {
+  return (
+    <div>
+
+    </div>
+  )
+}
