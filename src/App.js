a6a3ebb6d162a46beebedcb33a47d1196f65706c
claudia <mail@claudiapiccioni.it>
2020-06-05 16:47:04 +0200

    I defined the state of 'categories' and 'difficulties'using useState, and mapping both the DB arrays, in order to show the elements in the dropdown menu. It doesn't work yet since I didn't set the handleSubmit function. I want to get from that both category and difficulty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from "./FlashCardList";
 import "./app.css";
 import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState([])
+  const [categories, setCategories] = useState([])
+  const [difficulties, setDifficulties] = useState([])
+
+  const categoryEl = useRef()
+  const difficultyEl = useRef()
+
+  function handleSubmit(e) {
+
+  }
 
   // https://opentdb.com/api_config.php
   useEffect(() => {
@@ -18,7 +27,7 @@ function App() {
 
           return {
             id: `${index}-${Date.now()}`,
-            questions: "questionItem.question",
+            question: questionItem.question,
             answer: questionItem.correct_answer,
             options: options.sort(() => Math.random() - .5),
 
@@ -28,6 +37,28 @@ function App() {
   }, [])
   return (
     <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="difficulty">Difficulty</label>
+          <select id="difficulty" ref={difficultyEl}>
+            {difficulties.map(difficulty => {
+              return <option value={difficulty.id} key={difficulty.id}>{difficulty.name}</option>
+            })}
+
+          </select>
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
       <div className="container">
         <FlashcardList flashcards={flashcards} />
       </div>
