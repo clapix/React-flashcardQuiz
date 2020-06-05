39dd6d2dd817fbede3afb5ab78b81971e5e5be91
claudia <mail@claudiapiccioni.it>
2020-06-05 14:48:54 +0200

    installed axios and used the useEffect hook to connect to the API from the opendb.com free DB. I consol.got to check and to see the routes I'll need to parse the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from "./FlashCardList";
-import "./app.css"
+import "./app.css";
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
 
+  // https://opentdb.com/api_config.php
+  useEffect(() => {
+    axios
+      .get("https://opentdb.com/api.php?amount=10")
+      .then(res => {
+        console.log(res.data)
+      })
+  }, [])
 
   return (
     <>
